a7b9a4613b5e1f0947225b732cf96b8066b26b7f
Sean Yeo Degen <45349066+Seanydegen@users.noreply.github.com>
2022-10-12 14:31:27 +0800

    Complete Draft System Admin - Create User
    XML Layout - Create User

---
+++ app/src/main/java/com/example/crashcourse/sysAdmin/sysAdmin_createUser.java
@@ -0,0 +1,15 @@
+package com.example.crashcourse.sysAdmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.crashcourse.R;
+
+public class sysAdmin_createUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sysadmin_create_user);
+    }
+}
